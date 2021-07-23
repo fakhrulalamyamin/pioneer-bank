695f652e446bd9db36cbb75ad6a6dd2719fa7343
fakhrulalamyamin <fakhrulalamyamin1@gmail.com>
2021-07-23 18:57:22 +0600

    done

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ loginBtn.addEventListener('click', function(){
     transactinArea.style.display = 'block'
 })
 
-function updateSpan(id, depositNum) {
+function updateSpan(id, amount) {
     const current = document.getElementById(id).innerText
     const currentNum = parseFloat(current)
-    const total = currentNum + depositNum
+    // let total = 0;
+    // if (addOrSubs == 'add') {
+    //     total =  currentNum + amount
+    // } else if (addOrSubs == 'sub') {
+    //     total = currentNum - amount
+    // } else {
+    //     alert('Please mention deposit or withdraw')
+    // }
+    // document.getElementById(id).innerText = Math.abs(total)
+    const total = currentNum + amount;
+
     document.getElementById(id).innerText = total
 }
 
@@ -36,6 +46,8 @@ const withdrawBtn = document.getElementById('addWithdraw');
 withdrawBtn.addEventListener('click', function(){
     const withdrawNum = getInput('withdrawAmount')
     
-    
+    updateSpan('currentWithdraw', withdrawNum)
+    updateSpan('currentBalance', -1 * withdrawNum)
+
     document.getElementById('withdrawAmount').value = ''
 })
